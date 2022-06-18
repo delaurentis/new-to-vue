015285fcf5e21140b36f5129de20cc13d4905188
Peter DeLaurentis <pete@Peters-MacBook-Air.local>
2022-06-18 09:24:10 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="title">Hello World</h1>
+  <p class="description">
+    Welcome to your first web app.
+  </p>
+  <counter msg="Count is" />
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+  
+// Options API
+export default {
+  components: {
+    'counter': Counter,
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: black;
+}
+p {
+  margin-bottom: 24px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="component">
+    <button @click="incrementCount()">{{ msg }}: {{ count }}</button>
+  </div>
+
+</template>
+
+<script>
+
+// Options API
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++
+    },
+  },
+  mounted() {
+  },
+}
+
+</script>
+
+<style scoped>
+
+  button {
+    font-size: 14px;
+    opacity: 1;
+    padding: 10px;
+    background-color: #E5E5E5;
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  button:hover{
+   
+  }
+  button:active{
+    background-color: #DFDFDF;
+    transform: translate(1px, 1px);
+  }
+
+
+</style>
